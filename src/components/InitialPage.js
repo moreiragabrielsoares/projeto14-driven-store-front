bdf66575d605ad4c4cdc3cc21dd5f2e831ffda9a
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-07-06 21:46:33 -0300

    cria a estrutura de layout da página inicial

--- src/components/InitialPage.js
+++ src/components/InitialPage.js
@@ -12,6 +12,12 @@ import TopBar from "./TopBar";
 
 function InitialPage () {
 
+    const navigate = useNavigate();
+
+
+    function logIn () {
+        navigate("/login");
+    }
 
 
 
@@ -24,24 +30,105 @@ function InitialPage () {
 
                 <LineContainer>
 
-                    <LineTitle>CATEGORIA 1</LineTitle>
+                    <LineTitle>CADERNOS</LineTitle>
 
                     <ProductsContainer>
 
                         <ProductContainer>
 
-                            <ProductImg>IMG</ProductImg>
-                            <ProductName>Camisa Xablau</ProductName>
+                            <ProductImg>
+                                <img src="https://images.tcdn.com.br/img/img_prod/997151/caderno_universitario_capa_dura_naruto_1_materia_sd_4925_1_0976068f9c099ff2ada73892b3e07c39.jpg"/>
+                            </ProductImg>
+                            <ProductName>Caderno Narutin</ProductName>
                             <ProductPrice>R$100,00</ProductPrice>
-                            <AddToCartButton>Add to Cart</AddToCartButton>
+                            <AddToCartButton onClick={() => logIn()}>Adicionar ao Carrinho</AddToCartButton>
 
                         </ProductContainer>
 
                         <ProductContainer>
 
-                            <ProductImg>IMG</ProductImg>
+                            <ProductImg>
+                                <img src="https://images-americanas.b2w.io/produtos/4326277747/imagens/caderno-homem-aranha-1-materia-universitario-espiral-80-fls-tilibra/4326277747_1_large.jpg"/>
+                            </ProductImg>
                             <ProductName>NAME</ProductName>
                             <ProductPrice>PRICE</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://static.ocaderninho.com.br/produto/multifotos/hd/20210211171654_1769998231_DZ.jpg"/>
+                            </ProductImg>
+                            <ProductName>NAME</ProductName>
+                            <ProductPrice>PRICE</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://images.tcdn.com.br/img/img_prod/997151/caderno_universitario_capa_dura_naruto_1_materia_sd_4925_1_0976068f9c099ff2ada73892b3e07c39.jpg"/>
+                            </ProductImg>
+                            <ProductName>Caderno Narutin</ProductName>
+                            <ProductPrice>R$100,00</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+
+                    </ProductsContainer>
+
+                </LineContainer>
+
+                <LineContainer>
+
+                    <LineTitle>CANECAS</LineTitle>
+
+                    <ProductsContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://images.tcdn.com.br/img/img_prod/997151/caderno_universitario_capa_dura_naruto_1_materia_sd_4925_1_0976068f9c099ff2ada73892b3e07c39.jpg"/>
+                            </ProductImg>
+                            <ProductName>Caderno Narutin</ProductName>
+                            <ProductPrice>R$100,00</ProductPrice>
+                            <AddToCartButton onClick={() => logIn()}>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://static.ocaderninho.com.br/produto/multifotos/hd/20210211171654_1769998231_DZ.jpg"/>
+                            </ProductImg>
+                            <ProductName>NAME</ProductName>
+                            <ProductPrice>PRICE</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://static.ocaderninho.com.br/produto/multifotos/hd/20210211171654_1769998231_DZ.jpg"/>
+                            </ProductImg>
+                            <ProductName>NAME</ProductName>
+                            <ProductPrice>PRICE</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://images.tcdn.com.br/img/img_prod/997151/caderno_universitario_capa_dura_naruto_1_materia_sd_4925_1_0976068f9c099ff2ada73892b3e07c39.jpg"/>
+                            </ProductImg>
+                            <ProductName>Caderno Narutin</ProductName>
+                            <ProductPrice>R$100,00</ProductPrice>
                             <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
 
                         </ProductContainer>
@@ -52,24 +139,50 @@ function InitialPage () {
 
                 <LineContainer>
 
-                    <LineTitle>CATEGORIA 1</LineTitle>
+                    <LineTitle>CADERNOS</LineTitle>
 
                     <ProductsContainer>
 
                         <ProductContainer>
 
-                            <ProductImg>IMG</ProductImg>
-                            <ProductName>Camisa Xablau</ProductName>
+                            <ProductImg>
+                                <img src="https://images.tcdn.com.br/img/img_prod/997151/caderno_universitario_capa_dura_naruto_1_materia_sd_4925_1_0976068f9c099ff2ada73892b3e07c39.jpg"/>
+                            </ProductImg>
+                            <ProductName>Caderno Narutin</ProductName>
                             <ProductPrice>R$100,00</ProductPrice>
-                            <AddToCartButton>Add to Cart</AddToCartButton>
+                            <AddToCartButton onClick={() => logIn()}>Adicionar ao Carrinho</AddToCartButton>
 
                         </ProductContainer>
 
                         <ProductContainer>
 
-                            <ProductImg>IMG</ProductImg>
+                            <ProductImg>
+                                <img src="https://static.ocaderninho.com.br/produto/multifotos/hd/20210211171654_1769998231_DZ.jpg"/>
+                            </ProductImg>
                             <ProductName>NAME</ProductName>
                             <ProductPrice>PRICE</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://static.ocaderninho.com.br/produto/multifotos/hd/20210211171654_1769998231_DZ.jpg"/>
+                            </ProductImg>
+                            <ProductName>NAME</ProductName>
+                            <ProductPrice>PRICE</ProductPrice>
+                            <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+
+                        </ProductContainer>
+
+                        <ProductContainer>
+
+                            <ProductImg>
+                                <img src="https://images.tcdn.com.br/img/img_prod/997151/caderno_universitario_capa_dura_naruto_1_materia_sd_4925_1_0976068f9c099ff2ada73892b3e07c39.jpg"/>
+                            </ProductImg>
+                            <ProductName>Caderno Narutin</ProductName>
+                            <ProductPrice>R$100,00</ProductPrice>
                             <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
 
                         </ProductContainer>
@@ -94,26 +207,27 @@ export default InitialPage;
 const BodyContainer = styled.div`
     display: flex;
     flex-direction: column;
+    margin-top: 80px;
+    padding: 0 15px;
 `;
 
 const LineContainer = styled.div`
     display: flex;
     flex-direction: column;
-
-    @media (max-width: 600px) {
-        width: 500px;
-    }
+    margin-bottom: 25px;
 `;
 
 const LineTitle = styled.div`
     font-family: 'Righteous';
     font-size: 18px;
+    margin-bottom: 15px;
 `;
 
 const ProductsContainer = styled.div`
     display: flex;
     flex-direction: row;
-    width: 100%;
+    width:100%;
+    overflow-x: auto;
 `;
 
 const ProductContainer = styled.div`
@@ -121,9 +235,6 @@ const ProductContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
-    width: 200px;
-    height: 300px;
 
     margin: 0 5px;
 
@@ -134,28 +245,45 @@ const ProductContainer = styled.div`
         cursor: pointer;
         border-color: lightgray;
     }
+
+    min-width:180px;
+    width: 180px;
+    height: 300px;
+
+    @media (max-width: 600px) {
+        min-width: 180px;
+    }
 `;
 
 const ProductImg = styled.div`
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
+    img {
+        width: 90%;
+    }
 `;
 
 const ProductName = styled.div`
     font-family: 'Roboto';
     font-size: 14px;
     font-weight: 300;
+    margin-bottom: 10px;
 `;
 
 const ProductPrice = styled.div`
     font-family: 'Roboto';
     font-size: 18px;
     font-weight: 700;
+    margin-bottom: 10px;
 `;
 
 const AddToCartButton = styled.div`
     background-color: #6ACEC7;
     font-family: 'Roboto';
-    font-size: 18px;
+    font-size: 16px;
     font-weight: 700;
     color: #FFFFFF;
     display: flex;
